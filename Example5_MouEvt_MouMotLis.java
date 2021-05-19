25e16090318fae4f275265eaa46edd50e93354f0
Angelica Madrona <madronaangelica30@gmail.com>
2021-05-19 10:51:45 +0800

    Added Example 5, with error

---
+++ Example5_MouEvt_MouMotLis.java
@@ -0,0 +1,68 @@
+
+package midterm_project.Midterm_Project;
+import java.awt.*;
+import java.awt.event.*; 
+
+public class Example5_MouEvt_MouMotLis extends Frame {
+    private TextField tfMouseX;
+    private TextField tfMouseY;
+    
+    private TextField tfMousePositionX;
+    private TextField tfMousePositionY;
+    
+    public Example5_MouEvt_MouMotLis(){
+        setLayout(new FlowLayout());
+        
+        add(new Label("X-Click: "));
+        tfMouseX = new TextField(10);
+        tfMouseX.setEditable(false);
+        add(tfMouseX);
+        add(new Label("Y-Click: "));
+        tfMouseY = new TextField(10);
+        tfMouseY.setEditable(false);
+        add(tfMouseY);
+        
+        add(new Label("X-Position: "));
+        tfMousePositionX = new TextField(10);
+        tfMousePositionX.setEditable(false);
+        add(tfMousePositionX);
+        add(new Label("Y-Position: "));
+        tfMousePositionY = new TextField(10);
+        tfMousePositionY.setEditable(false);
+        add(tfMousePositionY);
+        
+        MyMouseListener listener = new MyMouseListener();
+        addMouseListener(listener);
+        addMouseMotionListener(listener);
+        
+        setTitle("Mouse Event");
+        setSize(400, 120);
+        setVisible(true);
+    }
+    
+    public static void main(String[] args){
+        new Example5_MouEvt_MouMotLis();
+    }
+    
+    public abstract class MyMouseListener implements MouseListener, MouseMotionListener{
+        @Override
+        public void mouseClicked(MouseEvent evt){
+            tfMouseX.setText(evt.getX() + "");
+            tfMouseY.setText(evt.getY() + "");
+        }
+        
+        public void mousePressed(WindowEvent evt) { }
+        public void mouseReleased(WindowEvent evt) { }
+        public void mouseEntered(WindowEvent evt) { }
+        public void mouseExited(WindowEvent evt) { }
+        
+        @Override
+        public void mouseMoved(MouseEvent evt){
+            tfMousePositionX.setText(evt.getX() + "");
+            tfMousePositionY.setText(evt.getY() + "");
+        }
+        
+        @Override public void mouseDragged(MouseEvent evt){ }
+    }
+}
+
