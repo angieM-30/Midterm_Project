f684b9d1414ed33776c53bb0f365b3c8f8494c19
Angelica Madrona <madronaangelica30@gmail.com>
2021-05-19 11:53:05 +0800

    Added Example10

---
+++ Example10_SameListIns_Buttons.java
@@ -0,0 +1,50 @@
+
+package midterm_project.Midterm_Project;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Example10_SameListIns_Buttons extends Frame {
+    private TextField tfCount;
+    private Button btnCountUp, btnCountDown, btnReset;
+    private int count = 0;
+    
+    public Example10_SameListIns_Buttons () { 
+        setLayout(new FlowLayout());
+        add(new Label("Counter"));
+        tfCount = new TextField("0", 10); 
+        tfCount.setEditable(false); add(tfCount);
+        
+        btnCountUp = new Button("Count Up"); 
+        add(btnCountUp); 
+        btnCountDown = new Button("Count Down"); 
+        add(btnCountDown); 
+        btnReset = new Button("Reset"); 
+        add(btnReset);
+        
+        AllButtonsListener listener = new AllButtonsListener();
+        
+        btnCountUp.addActionListener(listener); 
+        btnCountDown.addActionListener(listener);
+        btnReset.addActionListener(listener);
+        
+        setTitle("AWT Counter");
+        setSize(400, 100);
+        setVisible(true);
+    }
+    
+    public static void main(String[] args) { 
+        new Example10_SameListIns_Buttons();
+    }
+    
+    private class AllButtonsListener implements ActionListener { 
+        @Override
+    public void actionPerformed(ActionEvent evt) {
+        String btnLabel = evt.getActionCommand(); 
+        if (btnLabel.equals("Count Up")) {
+            ++count; } 
+        else if (btnLabel.equals("Count Down")) { 
+            --count; } else { count = 0; } 
+        tfCount.setText(count + ""); 
+        }
+    }
+}
